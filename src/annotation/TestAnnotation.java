05904748e065e30e001adf258a14012304136736
yangnk <yangningkai@qq.com>
2018-11-24 09:44:14 +0800

    修复之前注解无法显示的问题

--- src/annotation/TestAnnotation.java
+++ src/annotation/TestAnnotation.java
@@ -8,24 +8,24 @@ import java.lang.reflect.Method;
 public class TestAnnotation {
 	public static void main(String[] args) {
 		try {
-			//获取类上的注解
 			Class aClass = Class.forName("annotation.Child");
 //			//test
 //			Method[] methods = aClass.getMethods();
-//			for (Method method : methods) {
+//			for (Method m   ethod : methods) {
 //				System.out.println(method.getName());
 //			}
-
-			while (aClass.isAnnotationPresent(aClass)) {
-				Description description = (Description) aClass.getAnnotation(aClass);
+			//获取类上的注解
+			if (aClass.isAnnotationPresent(Description.class)) {
+				Description description = (Description) aClass.getAnnotation(Description.class);
 				System.out.println(description.desc());
 			}
 			//获取方法上的注解
 			Method[] methodArr = aClass.getMethods();
 			for (Method method1: methodArr) {
-				if (method1.isAnnotationPresent(aClass)) {
-					Description[] descriptionArr = (Description[]) method1.getAnnotations();
-					System.out.println(descriptionArr[0]);
+				if (method1.isAnnotationPresent(Description.class)) {
+					Description methodDescription = (Description) method1.getAnnotation(Description.class);
+					System.out.println("desc is :" +methodDescription.desc()+"\nauthor is :" +methodDescription.author()
+							+"\nage is :" +methodDescription.age());
 				}
 			}
 		} catch (ClassNotFoundException e) {
