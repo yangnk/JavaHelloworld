16e365ae883bf1a8842109109a4fcf7fc0e28d73
yangnk <yangningkai@qq.com>
2018-12-17 11:25:42 +0800

    新增了ThreadLocal的demo

---
+++ src/main/java/concurrent/ThreadLocalSample.java
@@ -0,0 +1,19 @@
+package concurrent;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author yangningkai
+ * @create 2018-12-16 下午9:47
+ **/
+public class ThreadLocalSample {
+    public static ThreadLocal threadLocal = new ThreadLocal();
+
+    public static void main(String[] args) {
+        if (threadLocal.get() == null) {
+            System.out.println("从咩有放过东西");
+            threadLocal.set("新的东西");
+        }
+        System.out.println(threadLocal.get());
+    }
+}
